1512636ff87e0a8a156e35ab7a56fe062805d844
manoj.d <manoj.d.929@gmail.com>
2018-06-19 22:48:53 +0530

    server side code

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-// const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const keys = require('./config/keys');
+
+// order for require statement is important here!!!
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, { user: keys.DB_USERNAME, pass: keys.DB_PASSWORD }); 
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World'});
-});
+// to set cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// cookie logic end
 
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server started, Listening at port ${PORT}...`)
+  console.log(`Server started, Listening at port ${PORT}...`);
 });
