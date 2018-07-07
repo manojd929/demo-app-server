6142b356b4924cdcbc3980258d5f8ca65af8c354
manoj.d <manoj.d.929@gmail.com>
2018-07-07 23:48:59 +0530

    heroku postbuild config

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+// Make sure express behaves correctly in production
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // Like out main.js file or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html frile
+  // If it does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started, Listening at port ${PORT}...`);
