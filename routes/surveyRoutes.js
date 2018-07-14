03773c7570a7cda6b65cd7019c0aeac7d46c48ed
manoj.d <manoj.d.929@gmail.com>
2018-07-14 22:13:17 +0530

    update survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,8 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+// url is integrated module inside nodejs system
+const { URL } = require('url');
+
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -8,6 +13,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for Voting!!!');
+  });
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
@@ -36,7 +45,46 @@ module.exports = app => {
     }
   });
 
-  app.get('api/surveys/thanks', requireLogin, (req, res) => {
-    res.send('Thanks so much for voting!');
+  app.post('/api/surveys/webhooks', (req, res) => {
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const parserObj = new Path('/api/surveys/:surveyId/:choice');
+        const pathname = new URL(url).pathname;
+
+        const match = parserObj.test(pathname);
+        if (match) {
+          return { email, ...match };
+        }
+      })
+      .compact(events)
+      .uniqBy(compactEvents, 'email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false },
+          }
+        }, {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec();
+      })
+      .value();
+
+    // we do not have to respond to sendGrid anything 
+
+    /* const events = _.map(req.body, ({ email, url }) => {
+      const parserObj = new Path('/api/surveys/:surveyId/:choice');
+      const pathname = new URL(url).pathname;
+
+      const match = parserObj.test(pathname); // either be an object or null
+      if (match) {
+        return { email, ...match }; // object containing email, surveyId, choice as keys
+      }
+    });
+
+    const compactEvents = _.compact(events); // removes undefined events from the events list
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId'); */
   });
 };
